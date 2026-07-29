d57274d8ed99fcf4330202c30b1ba1c07cadd6f8
julesthornton312 <julesthornton@icloud.com>
2026-07-29 11:15:48 -0400

    Refactor DQN1 and remove DQN2 class
    Refactor DQN1 class to use consistent naming for sequential model and remove DQN2 class.

--- DQN/DQN_model.py
+++ DQN/DQN_model.py
@@ -3,29 +3,15 @@ import torch.nn as nn
 
 class DQN1(nn.Module):
   def __init__(self):
-    super().__init__(self)
-    DQS_seq1 = nn.Sequential()
-    DQS_seq1.append(nn.Linear(96, 128))
-    DQS_seq1.append(nn.ReLU())
-    DQS_seq1.append(nn.Linear(128, 64))
-    DQS_seq1.append(nn.ReLU())
-    DQS_seq1.append(nn.Linear(64, 16))
-    DQS_seq1.append(nn.ReLU())
-    DQS_seq1.append(nn.Linear(16, 9))
+    super().__init__()
+    DQS_seq = nn.Sequential()
+    DQS_seq.append(nn.Linear(96, 128))
+    DQS_seq.append(nn.ReLU())
+    DQS_seq.append(nn.Linear(128, 64))
+    DQS_seq.append(nn.ReLU())
+    DQS_seq.append(nn.Linear(64, 16))
+    DQS_seq.append(nn.ReLU())
+    DQS_seq.append(nn.Linear(16, 9))
   def forward(self, x):
     return DQS_seq1(x)
 
-class DQN2(nn.Module):
-  def __init(self):
-    super().__init__(self)
-    DQS_seq2 = nn.Sequential()
-    DQS_seq2.append(nn.Linear(96, 128))
-    DQS_seq2.append(nn.ReLU())
-    DQS_seq2.append(nn.Linear(128, 64))
-    DQS_seq2.append(nn.ReLU())
-    DQS_seq2.append(nn.Linear(64, 16))
-    DQS_seq2.append(nn.ReLU())
-    DQS_seq2.append(nn.Linear(16, 9))
-  def forward(self, x):
-    return DQS_seq2(x)
-    
