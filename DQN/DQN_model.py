c96722210681d570ef940d4fbf7d110d4cef0858
julesthornton312 <julesthornton@icloud.com>
2026-07-29 11:17:07 -0400

    Fix DQN model to use instance variable for sequential

--- DQN/DQN_model.py
+++ DQN/DQN_model.py
@@ -4,14 +4,14 @@ import torch.nn as nn
 class DQN1(nn.Module):
   def __init__(self):
     super().__init__()
-    DQS_seq = nn.Sequential()
-    DQS_seq.append(nn.Linear(96, 128))
-    DQS_seq.append(nn.ReLU())
-    DQS_seq.append(nn.Linear(128, 64))
-    DQS_seq.append(nn.ReLU())
-    DQS_seq.append(nn.Linear(64, 16))
-    DQS_seq.append(nn.ReLU())
-    DQS_seq.append(nn.Linear(16, 9))
+    self.DQS_seq = nn.Sequential()
+    self.DQS_seq.append(nn.Linear(96, 128))
+    self.DQS_seq.append(nn.ReLU())
+    self.DQS_seq.append(nn.Linear(128, 64))
+    self.DQS_seq.append(nn.ReLU())
+    self.DQS_seq.append(nn.Linear(64, 16))
+    self.DQS_seq.append(nn.ReLU())
+    self.DQS_seq.append(nn.Linear(16, 9))
   def forward(self, x):
-    return DQS_seq1(x)
+    return self.DQS_seq(x)
 
