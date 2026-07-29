007bfd7cc6df3d41fe02a68f8c8a9219cf4bab05
julesthornton312 <julesthornton@icloud.com>
2026-07-29 10:12:04 -0400

    Initialize DQN training script with models and parameters

---
+++ DQN_train.py
@@ -0,0 +1,19 @@
+import torch
+import numpy as np
+from DQN_model import DQN1
+from DQN_model import DQN2
+
+
+epsilon = 1
+epsilon_decay = 0.9
+epsilon_min = 0.05
+lr = 0.0001
+discount = 0.9
+num_episodes = 100000
+
+network1 = DQN1()
+network2 = DQN2()
+
+optimizer1 = torch.optim.Adam(network1.parameters(), lr)
+optimizer2 = torch.optim.Adam(network2.parameters(), lr)
+criterion = torch.nn.MSELoss()
